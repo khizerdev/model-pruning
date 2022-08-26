845b246ca4882f39115fb09d785f00df48ba91a7
khizerdev <46632721+khizerdev@users.noreply.github.com>
2022-08-26 12:44:03 +0500

    Pruning models via model

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Prunable;
+use Illuminate\Database\Eloquent\Builder;
+
 
 class Article extends Model
 {
     use HasFactory;
+    use Prunable;
+
+
+    public function prunable(): Builder
+    {
+        return static::where('created_at' , '<=' , now()->subWeek());
+    }
 }
